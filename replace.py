cc7c1dd91ef835acf0aea7c4e15ed914e9e59065
Jisheng Jiang <jiangjs03@gmail.com>
2024-08-01 22:10:31 -0700

    oppo findx6 pro new config been update

--- replace.py
+++ replace.py
@@ -34,7 +34,7 @@ def replace_text_in_file(file_path):
         file.write(content)
 
 # file path
-file_path = r'C:\Users\User\Desktop\TheFiles\CameraConfigPro\oppo FindX7 Ultra.agc'
+file_path = r'C:\Users\User\Desktop\TheFiles\CameraConfigPro\oppo series phone\oppo FindX6 Pro.agc'
 
 # excute it
 replace_text_in_file(file_path)
