5fcc9544de029a0a6d7e3bee11952113fd1883b5
Jisheng Jiang <jiangjs03@gmail.com>
2024-08-01 21:55:24 -0700

    Oppo general series updated

--- replace.py
+++ replace.py
@@ -2,7 +2,19 @@ import re
 
 # Difined Dict
 translation_dict = {
-
+    "夜视仪": "Night Vision PRO",
+    "徕卡灵动": "Leica Vivid",
+    "徕卡柔和": "Leica Soft",
+    "徕卡鲜艳": "Leica Spirited",
+    "复古胶片": "Vintage Film",
+    "自然人像": "Natural Portrait",
+    "电影叙事": "Cinematic Filter",
+    "极致青橙": "Cyan and Orange",
+    "哈苏自然": "Hasselblad Natural",
+    "富士模式": "Fuji Mode",
+    "黑白人文": "Black and White Humanistic",
+    "印象怀旧": "Nostalgic Impression",
+    "酷灰质感": "Cool Gray Texture"
 }
 
 # counter
@@ -22,7 +34,7 @@ def replace_text_in_file(file_path):
         file.write(content)
 
 # file path
-file_path = r'C:\Users\User\Desktop\TheFiles\CameraConfigPro\Galaxy config(solve problem with focus).agc'
+file_path = r'C:\Users\User\Desktop\TheFiles\CameraConfigPro\oppo general config.agc'
 
 # excute it
 replace_text_in_file(file_path)
